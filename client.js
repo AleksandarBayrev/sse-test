7d5dfc9d6bd1428ae834494c334ab287ce4a84a5
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2025-04-18 19:53:06 +0300

    Small update

--- client.js
+++ client.js
@@ -5,11 +5,8 @@ const data = new EventSource("http://localhost:3000/weatherforecasts");
 data.addEventListener("open", (message) => {
     console.log(message);
 });
-data.addEventListener("message", (message) => {
-    console.log(message.data);
-});
 data.addEventListener("server_logs", (message) => {
-    console.log(message.data);
+    console.log(message, message.data);
 });
 data.addEventListener("error", (message) => {
     console.log(message);
